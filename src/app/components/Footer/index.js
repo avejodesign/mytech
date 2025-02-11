35d8e233c62785152a902ee8b187767bd15a142b
Ricardo <avejo.design@gmail.com>
2025-02-11 12:56:29 -0300

    feat: fix trigger of footer

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -10,7 +10,7 @@ export default function Footer() {
     useGSAP(() => {
         gsap.timeline({
             scrollTrigger: {
-                trigger: ".accordion",
+                trigger: ".group-footer",
                 start: "top bottom",
                 scrub: false,
             }
